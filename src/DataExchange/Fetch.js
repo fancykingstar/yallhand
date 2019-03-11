596babe4e2e58e6046f6543c72d2f5dcd1525362
PommeMicrobe <vincent.veysset@adfab.fr>
2019-03-11 13:29:48 -0400

    Add deployment configuration

--- src/DataExchange/Fetch.js
+++ src/DataExchange/Fetch.js
@@ -1,6 +1,6 @@
 import { UIStore } from "../Stores/UIStore"
 // const api_url = "http://127.0.0.1:3000/";
-const api_url = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : "https://api.quadrance-develop.fabtesting.com/"
+const api_url = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : "http://127.0.0.1:3000/"
 // const api_url = "http://ec2-3-86-251-153.compute-1.amazonaws.com/"
 
 export const getUser = () => {
