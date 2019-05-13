3e6ce6a1eaa0e4afa7c2c08810ebdd14a4e96279
Zuppe <mark@aubry.ai>
2019-05-13 18:02:55 -0400

    super admin api auth

--- src/SuperAdmin/Down.js
+++ src/SuperAdmin/Down.js
@@ -1,10 +1,9 @@
+import {getHeaders} from "../DataExchange/Fetch"
+
 const axios = require('axios');
 const base = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : "http://127.0.0.1:3000/"
 
 export const api_get = (endpoint) => {
-   return axios({method: "get", url: base + endpoint, auth: {
-        username: 'quadrance',
-        password: 'Quadrance2019!'
-    }})
+   return axios({method: "get", headers: getHeaders(), url: base + endpoint, })
 }
 
