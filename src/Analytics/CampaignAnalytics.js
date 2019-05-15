783ab7ea95aeddab704bdde5b66e72cc1c15a72b
Zuppe <mark@aubry.ai>
2019-05-15 14:22:43 -0400

    fixed search to disregard char casing and fixed and added sorting for analytic tables

--- src/Analytics/CampaignAnalytics.js
+++ src/Analytics/CampaignAnalytics.js
@@ -2,14 +2,45 @@ import React from "react";
 import {inject, observer} from "mobx-react"
 import UTCtoFriendly from "../SharedCalculations/UTCtoFriendly"
 import {giveMeKey} from "../SharedCalculations/GiveMeKey"
-import { Table, Header, Button, Modal} from "semantic-ui-react";
+import { Table, Header, Button, Modal,Icon} from "semantic-ui-react";
+import { SearchBox } from "../SharedUI/SearchBox"
 import {getContentObj} from "../SharedCalculations/GetContentObj"
 
-@inject("AccountStore", "EmailStore", "TeamStore", )
+@inject("AccountStore", "EmailStore", "TeamStore", "UIStore")
 @observer
 export class CampaignAnalytics extends React.Component {
+  constructor(props){
+    super(props)
+    const {AccountStore} = this.props
+    this.clickRate = (camp) => Number.isNaN(Math.round(camp.clicks / camp.total_views * 100))? 0 : Math.round(camp.clicks / camp.total_views * 100)
+    this.sort = (controller, direction) => {
+      const param = controller
+      if(direction === "Lowest") {
+        if(param === "clicks") AccountStore.loadAnalyticData_campaigns(AccountStore.analyticData_campaigns.slice().sort((a,b) => (this.clickRate(a) > this.clickRate(b)? 1 : -1)))
+        else AccountStore.loadAnalyticData_campaigns(AccountStore.analyticData_campaigns.slice().sort((a,b) => (a[param] > b[param])? 1 : -1))
+      }
+
+
+      else {
+        if(param === "clicks") AccountStore.loadAnalyticData_campaigns(AccountStore.analyticData_campaigns.slice().sort((a,b) => (this.clickRate(a) < this.clickRate(b)? 1 : -1)))
+        else AccountStore.loadAnalyticData_campaigns(AccountStore.analyticData_campaigns.slice().sort((a,b) => (a[param] < b[param])? 1 : -1))
+          }
+  }
+  }
+  componentDidMount(){
+    this.sort("sent", "Highest")
+  }
   render() {
-    const {AccountStore, EmailStore, TeamStore} = this.props
+    const {AccountStore, EmailStore, TeamStore, UIStore} = this.props
+
+
+    
+
+    const searchFilter = (all) => {
+      if(UIStore.search.campaignsSearchValue === "") return all
+      else return all.filter(i => i.subject.toLowerCase().includes(UIStore.search.campaignsSearchValue.toLowerCase()))
+  }
+
     const getCampaignRecipients = (id) => {
       const targets = EmailStore._getCampaign(id)
       if(!targets) return;
@@ -19,12 +50,12 @@ export class CampaignAnalytics extends React.Component {
         : `${TeamStore._getTeam(targets.teamID).label} ${targets.tags.length === 0? "No Tags" : TeamStore._getTag(targets.tags[0]).label}`
       }
     }
-    const outbounds = AccountStore.analyticData_campaigns.map(camp => {
+    const outbounds = searchFilter(AccountStore.analyticData_campaigns).map(camp => {
       const campaign = EmailStore._getCampaign(camp.campaignID)
 
       if(!campaign) return null
       return (
-        <Table.Row disabled={!camp.completed} key={"camp" + giveMeKey()}>
+        <Table.Row disabled={!camp.completed? EmailStore._getCampaign(camp.campaignID).isTriggered? false:true : false} key={"camp" + giveMeKey()}>
           <Table.Cell>
             <Header>
               <Header.Content>
@@ -35,7 +66,7 @@ export class CampaignAnalytics extends React.Component {
           <Table.Cell>{UTCtoFriendly(camp.sent)}</Table.Cell>
           <Table.Cell>{`${camp.total_views}/${camp.unique_views}`}</Table.Cell>
           <Table.Cell>{camp.open_rate}%</Table.Cell>
-          <Table.Cell>{Number.isNaN(Math.round(camp.clicks / camp.total_views * 100))? 0 : Math.round(camp.clicks / camp.total_views * 100)}%</Table.Cell>
+          <Table.Cell>{this.clickRate(camp)}%</Table.Cell>
           <Table.Cell></Table.Cell>
           <Table.Cell> 
             <Modal trigger={ <Button basic>Details</Button>}>
@@ -70,16 +101,17 @@ export class CampaignAnalytics extends React.Component {
           as="h2"
           content="Email Campaign Performance"
         />
+                   <div style={UIStore.responsive.isMobile? null : {float: 'right', paddingRight: 10, paddingBottom: 15,display: "inline-block"}}>     <SearchBox value={UIStore.search.campaignsSearchValue} output={val => UIStore.set("search", "campaignsSearchValue", val)}/></div>
+     
           <Table padded="very" basic="very">
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Sent</Table.HeaderCell>
-                <Table.HeaderCell>Views (All/Unique)</Table.HeaderCell>
-                <Table.HeaderCell>Open Rate</Table.HeaderCell>
-                <Table.HeaderCell>Click Rate</Table.HeaderCell>
+                <Table.HeaderCell rowSpan='2'>Name</Table.HeaderCell>
+                <Table.HeaderCell>Sent <br/><span> <Icon size="small" name="arrow up" onClick={e => this.sort("sent", "Highest")}/> <Icon size="small" name="arrow down" onClick={e => this.sort("sent", "Lowest")}/></span></Table.HeaderCell>
+                <Table.HeaderCell>Views (All/Unique) <br/><span> <Icon size="small" name="arrow up" onClick={e => this.sort("total_views", "Highest")}/> <Icon size="small" name="arrow down" onClick={e => this.sort("total_views", "Lowest")}/></span></Table.HeaderCell>
+                <Table.HeaderCell>Open Rate <br/><span> <Icon size="small" name="arrow up" onClick={e => this.sort("open_rate", "Highest")}/> <Icon size="small" name="arrow down" onClick={e => this.sort("open_rate", "Lowest")}/></span></Table.HeaderCell>
+                <Table.HeaderCell>Click Rate <br/><span> <Icon size="small" name="arrow up" onClick={e => this.sort("clicks", "Highest")}/> <Icon size="small" name="arrow down" onClick={e => this.sort("clicks", "Lowest")}/></span></Table.HeaderCell>
                 <Table.HeaderCell></Table.HeaderCell>
-               
                 <Table.HeaderCell />
               </Table.Row>
             </Table.Header>
