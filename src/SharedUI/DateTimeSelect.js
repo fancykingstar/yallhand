3566d5b9d619867a3843ef8d291cdff7f0f01572
Veraprot <vera.protopopova@gmail.com>
2019-07-01 11:41:08 -0400

    disable choosing past times

--- src/SharedUI/DateTimeSelect.js
+++ src/SharedUI/DateTimeSelect.js
@@ -9,64 +9,50 @@ import moment from "moment"
 export class DateTimeSelect extends React.Component { 
   constructor(props){
     super(props)
-    const now = moment();
+    const now = moment()
     const remainder = 15 - (now.minute() % 15);
-    const dateTime = moment(now).add(remainder, "minutes");
-    const start = this.props.notToday === undefined? dateTime: dateTime.add(1, 'day');
-    this.state={current: this.props.includeTime !== undefined? start:start.startOf('day')};
-    this.props.value(this.state.current.valueOf());
-  }
- 
-  render() {
-      
-   const yesterday = moment().subtract( 1, 'day' );
-    const validDates = ( current )=> current.isAfter( this.props.notToday === undefined? yesterday: moment() );
-    const validTimes = () => 
-      // this.state.current.isSame(new Date(), "day")? {hours:{min: moment().format('H')},minutes: {step: 5}}:
-      ({minutes: {step: 15}})
 
-    const getValidTimes = (dateTime) => {
-      if (moment().isSame(dateTime, 'day')) {
-        return {
-          hours: {
-            min: moment().format('H'),
-            max: 23,
-            step: 1,
-          },
-          minutes: {
-            min: moment().format('M'),
-            max: 59,
-            step: 15,
-          },
-        };
-      }
-    
+    this.state = {
+      currentDate: moment(now).add(remainder, "minutes")
+    }
+  }
+  
+  validTimeConstraints () {
+    if(moment().isSame(this.state.currentDate, 'day')) {
+      console.log('today do something diferently')
       return {
         hours: {
-          min: 0,
+          min: this.state.currentDate.hours(),
           max: 23,
           step: 1,
         },
         minutes: {
-          min: 0,
+          min: this.state.currentDate.minutes(),
           max: 59,
-          step: 15,
+          step: 5,
         },
       };
     }
+    return {
+      hours: { min: 1, max: 23, step: 1 }, 
+      minutes: {min: 0, max: 59, step: 15}
+    }
+  }
 
+  updateDate(newDateTime) {
+    this.setState({
+      currentDate: newDateTime
+    })
+  }
+
+  render() {
     return ( 
         <React.Fragment>
           <DateTime 
-          isValidDate={validDates}
-          timeConstraints={ getValidTimes(this.state.dateTime) } 
-          timeFormat={this.props.includeTime !== undefined}
-          defaultValue={this.state.current}
-          onChange={e => {
-            const newDate = e.isSame(new Date(), "day")? moment():e
-            this.setState({current: newDate})
-            this.props.value(moment.utc(newDate).valueOf())
-          }}
+          timeConstraints={this.validTimeConstraints()}
+          defaultValue={this.state.currentDate}
+          value={this.state.currentDate}
+          onChange={e => this.updateDate(e)}
           />
 
       </React.Fragment>
