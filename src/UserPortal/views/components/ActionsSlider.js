ebc97e0b7ee6184886c64a7e3832090a833db77b
Sanket <sanketk478@gmail.com>
2019-10-18 18:26:48 +0530

    Update ActionsSlider.js

--- src/UserPortal/views/components/ActionsSlider.js
+++ src/UserPortal/views/components/ActionsSlider.js
@@ -3,8 +3,8 @@ import { Container } from 'reactstrap';
 import Layout from '../../layouts/DefaultLayout';
 
 import Slider from "react-slick";
-import IconBox from "../components/IconBox";
-import ActionsForm from "../components/ActionsForm";
+import IconBox from "./IconBox";
+import ActionsForm from "./ActionsForm";
 import IconButton from '@material-ui/core/IconButton';
 
 import ActionsData from '../../data/actions.json';
@@ -25,11 +25,9 @@ class ActionSlider extends React.Component {
       this.state = {
          slideIndex: 0,
          updateCount: 0,
-         showActionForm: false,
-         showActionFormCompany: false,
+         showActionFormBlock: false,
          ActionsData: [],
          selectedActionData: {},
-         selectedActionDataCompany: {},
          colleague: 'Dylan Spencer',
          props_for: 'Teamwork',
          description: '',
@@ -49,79 +47,21 @@ class ActionSlider extends React.Component {
    showActionForm(item) {
       this.slider.slickGoTo(1);
       this.setState({
-         showActionForm: true,
+         showActionFormBlock: true,
          selectedActionData: item
       });
    }
-   showActionFormCompany(item) {
-      this.sliderCompany.slickGoTo(1);
-      this.setState({
-         showActionFormCompany: true,
-         selectedActionDataCompany: item
-      });
-   }
+
    hideActionForm() {
       this.slider.slickGoTo(0);
-      this.setState({ showActionForm: false });
+      this.setState({ showActionFormBlock: false });
    }
-   hideActionFormCompany() {
-      this.sliderCompany.slickGoTo(0);
-      this.setState({ showActionFormCompany: false });
-   }
-   handleInputChange(evt) {
-      const value =
-         evt.target.type === "checkbox" ? evt.target.checked : evt.target.value;
-      this.setState({
-         [evt.target.name]: value
-      });
-   }
+
    handleActionFormSubmit(data) {
       console.log(data);
    }
    render() {
-      const { FeaturedActions, generalActions, CompanyActions } = this.state
-      const settings = {
-         dots: false,
-         infinite: false,
-         slidesToShow: 5,
-         slidesToScroll: 1,
-         nextArrow: <SampleNextArrow />,
-         prevArrow: <SamplePrevArrow />,
-
-         responsive: [
-            {
-               breakpoint: 1600,
-               settings: {
-                  slidesToShow: 4,
-               }
-            },
-            {
-               breakpoint: 1440,
-               settings: {
-                  slidesToShow: 4,
-               }
-            },
-            {
-               breakpoint: 1280,
-               settings: {
-                  slidesToShow: 3,
-               }
-            },
-
-            {
-               breakpoint: 800,
-               settings: {
-                  slidesToShow: 2,
-               }
-            },
-            {
-               breakpoint: 480,
-               settings: {
-                  slidesToShow: 1,
-               }
-            }
-         ]
-      };
+      const { generalActions } = this.state
       const settings_multi = {
          dots: false,
          infinite: false,
@@ -166,15 +106,13 @@ class ActionSlider extends React.Component {
       };
       const settings_components_slide = {
          dots: false,
-         // draggable: false,
+         draggable: false,
          arrows: false,
          infinite: false,
          speed: 500,
          swipeToSlide: false,
-         // swipe: false,
          slidesToShow: 1,
          slidesToScroll: 1,
-
          afterChange: () =>
             this.setState(state => ({ updateCount: state.updateCount + 1 })),
          beforeChange: (current, next) => this.setState({ slideIndex: next })
@@ -195,27 +133,27 @@ class ActionSlider extends React.Component {
                               micon="star"
                               user_img={Star}
                               title={"Open Enrollment: Ask Anything"} showAction={() => { this.showActionForm({ label: "Open Enrollment: Ask Anything", img: Star }) }} />
-                           {/* {(generalActions) ? generalActions.map((item, index) => {
-                       var img = item.img;
-                       if (index === 0) { img = Star; }
-                       if (index === 1 || index === 4) { img = RefereCandidate; }
-                       if (index === 2 || index === 7) { img = AskManagement; }
-                       if (index === 3) { img = anonymousReport; }
-                       if (index === 5) { img = compensationReview; }
-                       if (index === 6 || index >= 8) { img = reportSomething; }
-                       return <IconBox
-                          key={index}
-                          micon="star"
-                          user_img={img}
-                          title={item.label} showAction={() => { this.showActionForm({ label: item.label, img: img }) }} />
-                    }) : ('')} */}
+                           {(generalActions) ? generalActions.map((item, index) => {
+                              var img = item.img;
+                              if (index === 0) { img = Star; }
+                              if (index === 1 || index === 4) { img = RefereCandidate; }
+                              if (index === 2 || index === 7) { img = AskManagement; }
+                              if (index === 3) { img = anonymousReport; }
+                              if (index === 5) { img = compensationReview; }
+                              if (index === 6 || index >= 8) { img = reportSomething; }
+                              return <IconBox
+                                 key={index}
+                                 micon="star"
+                                 user_img={img}
+                                 title={item.label} showAction={() => { this.showActionForm({ label: item.label, img: img }) }} />
+                           }) : ('')}
 
                         </Slider>
                      </div>
                   </div>
 
                </Container>
-               <Container id="action-form" elevation={4} className="action-form">
+               <Container elevation={4} className="action-form">
                   <ActionsForm
                      onSubmit={this.handleActionFormSubmit}
                      onCancel={this.hideActionForm.bind(this)}
