16b26638b8ffb32f390499adbc9b80e3d40c2c8c
mark zuppe <markzuppe@MacBook-Pro.local>
2019-12-30 21:57:09 -0500

    react lazy load opacitiy test

--- src/SharedUI/LazyImg.js
+++ src/SharedUI/LazyImg.js
@@ -5,10 +5,11 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 export const LazyImg = (props) => (
     <LazyLoadImage
       alt={props.alt}
-      effect="blur"
+      effect="opacity"
       height={props.height}
       width={props.width}
       src={props.src}
+      // placeholderSrc={props.src}
       style={props.style}
       />
   );
