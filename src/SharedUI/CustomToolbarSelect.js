ccc2f1dc475a5520b2a72e834a89ef6c8b665dfc
websky112 <tholt7607@gmail.com>
2020-01-08 16:54:22 +0000

    implemented feature for task

--- src/SharedUI/CustomToolbarSelect.js
+++ src/SharedUI/CustomToolbarSelect.js
@@ -1,75 +1,69 @@
-import React from "react";
-import IconButton from "@material-ui/core/IconButton";
-import Tooltip from "@material-ui/core/Tooltip";
-import StarIcon from "@material-ui/icons/Star";
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import DeleteIcon from "@material-ui/icons/Delete";
-import ArchiveIcon from '@material-ui/icons/Archive';
-import FilterIcon from "@material-ui/icons/FilterList";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import { withStyles } from '@material-ui/core/styles';
 
 const defaultToolbarSelectStyles = {
-
   iconStyle: {
-    display: "inline-block",
-    position: "relative",
-    transform: "translate(-10px, -40%)",
-    top: "20px",
-    color: "#8F8F8F"
+    display: 'inline-block',
+    position: 'relative',
+    transform: 'translate(-10px, -40%)',
+    top: '20px',
+    color: '#8F8F8F',
   },
-
   featured: {
-    fontSize: "1rem",
-    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
-    fontWeight: "400",
-    lineHeight: "1.75",
-    letterSpacing: "0.00938em",
-    color: "rgba(0, 0, 0, 0.87)",
-  }
+    fontSize: '1rem',
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontWeight: '400',
+    lineHeight: '1.75',
+    letterSpacing: '0.00938em',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
 };
 
 class CustomToolbarSelect extends React.Component {
-    
-  handleClick(val){
-    this.props.handleClick(val, this.props.selectedRows)
-  };
-
-  hasPublished(){
-      const selectedKeys = Object.keys(this.props.selectedRows.lookup);
-      const selectedContent = this.props.data.filter((content,i)=> selectedKeys.includes(String(i)));
-      return Boolean(selectedContent.filter(i=>i.everPublished).length);
+  handleClick(val) {
+    const { handleClick, selectedRows } = this.props;
+    handleClick(val, selectedRows);
   }
 
-  hasFeatured(){
-    const selectedKeys = Object.keys(this.props.selectedRows.lookup);
-    const selectedContent = this.props.data.filter((content,i)=> selectedKeys.includes(String(i)));
-    return Boolean(selectedContent.filter(i=>i.featured).length);
+  hasPublished() {
+    const { data, selectedRows } = this.props;
+    const selectedKeys = Object.keys(selectedRows.lookup);
+    const selectedContent = data.filter((content, i) => selectedKeys.includes(String(i)));
+    return Boolean(selectedContent.filter(i => i.everPublished).length);
   }
 
-
+  hasFeatured() {
+    const { data, selectedRows } = this.props;
+    const selectedKeys = Object.keys(selectedRows.lookup);
+    const selectedContent = data.filter((content, i) => selectedKeys.includes(String(i)));
+    return Boolean(selectedContent.filter(i => i.featured).length);
+  }
 
   render() {
     const { classes } = this.props;
+    const isFeatured = this.hasFeatured();
 
     return (
-      <div className={"custom-toolbar-select"}>
-        <Tooltip title={this.hasFeatured()? "Remove Feature":"Feature" }>
-          <IconButton className={classes.iconStyle} onClick={()=> this.handleClick(this.hasFeatured()? "remove feature":"feature")}>
-           {/*{this.hasFeatured()?  <StarBorderIcon className={classes.starIcon} />: <StarIcon className={classes.starIcon} />}*/}
-           {this.hasFeatured()?  <span className={classes.featured}>Feature</span>: <span className={classes.featured}>Feature</span>}
+      <div className="custom-toolbar-select">
+        <Tooltip title={isFeatured ? 'Remove Feature' : 'Feature'}>
+          <IconButton
+            className={classes.iconStyle}
+            onClick={() => this.handleClick(isFeatured ? 'remove feature' : 'feature')}
+          >
+            {isFeatured ? (
+              <span className={classes.featured}>Feature</span>
+            ) : (
+              <span className={classes.featured}>Feature</span>
+            )}
           </IconButton>
         </Tooltip>
-        {/* <Tooltip title={this.hasPublished()? "Archive":"Delete"}>
-          <IconButton className={classes.iconStyle} onClick={()=>this.handleClick("archive")}>
-           {this.hasPublished()? <ArchiveIcon className={classes.archiveIcon} /> :   <DeleteIcon className={classes.deleteIcon} />}
-          </IconButton>
-        </Tooltip> */}
-       
       </div>
     );
   }
 }
 
 export default withStyles(defaultToolbarSelectStyles, {
-  name: "CustomToolbarSelect"
+  name: 'CustomToolbarSelect',
 })(CustomToolbarSelect);
--- src/Tasks/TaskFrame.js
+++ src/Tasks/TaskFrame.js
@@ -1,19 +1,24 @@
-import React from "react";
-import { inject, observer } from "mobx-react";
-import { withRouter } from "react-router-dom";
-import { Button, Icon, Header } from "semantic-ui-react";
-import MUIDataTable from "mui-datatables";
-import styled from "styled-components";
-import { TaskStore } from "../Stores/TaskStore";
-import { AccountStore} from "../Stores/AccountStore";
-import UTCtoFriendly from "../SharedCalculations/UTCtoFriendly"
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+import MUIDataTable from 'mui-datatables';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import StarRoundedIcon from '@material-ui/icons/StarRounded';
+import Chip from '@material-ui/core/Chip';
+import { Button, Icon, Header } from 'semantic-ui-react';
+import styled from 'styled-components';
 
+import { AccountStore } from '../Stores/AccountStore';
+import UTCtoFriendly from '../SharedCalculations/UTCtoFriendly';
+import CustomToolbarSelect from '../SharedUI/CustomToolbarSelect';
+import { modifySurvey } from '../DataExchange/Up';
+
+@inject('TaskStore')
+@observer
 class TaskFrame extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { width: 0, height: 0 };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.state = { width: 0 };
   }
 
   componentDidMount() {
@@ -25,52 +30,72 @@ class TaskFrame extends React.Component {
     window.removeEventListener('resize', this.updateWindowDimensions);
   }
 
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  }
+  updateWindowDimensions = () => {
+    this.setState({ width: window.innerWidth });
+  };
 
   getMuiTheme = () =>
     createMuiTheme({
       overrides: {
         MUIDataTableBodyCell: {
           root: {
-            fontFamily: "Lato",
-            fontSize: "1em"
-          }
+            fontFamily: 'Lato',
+            fontSize: '1em',
+          },
         },
         MUIDataTableBodyRow: {
           root: {
-            zIndex: "1 !important"
-          }
+            zIndex: '1 !important',
+          },
         },
         MUIDataTableSelectCell: {
-            fixedHeader: {
-              zIndex: "1 !important"
-            },
-            headerCell: {
-              zIndex: "1 !important"
-            }
+          fixedHeader: {
+            zIndex: '1 !important',
           },
-          MUIDataTableHeadCell: {
-            fixedHeader: {
-              // position: "relative"
-              zIndex: "1 !important"
-            }
+          headerCell: {
+            zIndex: '1 !important',
+          },
+        },
+        MUIDataTableHeadCell: {
+          fixedHeader: {
+            // position: "relative"
+            zIndex: '1 !important',
+          },
         },
         MUIDataTable: {
           root: {
-            backgroundColor: "#FF000"
+            backgroundColor: '#FF000',
           },
           paper: {
-            boxShadow: "none",
-            border: "2px solid #e3e8ee",
-            borderRadius: 8
-          }
-        }
-      }
+            boxShadow: 'none',
+            border: '2px solid #e3e8ee',
+            borderRadius: 8,
+          },
+        },
+      },
     });
-  
+
+  handleClick = task => {
+    const { history } = this.props;
+    history.push(`/panel/tasks/manage-task/${task ? task.surveyID : ''}`);
+  };
+
+  handleFeatured = async (action, tableinfo) => {
+    const { TaskStore } = this.props;
+    const {accountID} = AccountStore.account;
+    tableinfo.data.forEach(async i => {
+      const ID = TaskStore.allTasks[i.dataIndex].surveyID;
+      await modifySurvey({
+        accountID,
+        surveyID: ID,
+        type: 'task',
+        featured: action === 'feature',
+      });
+    });
+  };
+
   render() {
+    const { TaskStore } = this.props;
     const MenuContainer = styled.div`
       display: flex;
       flex-wrap: wrap;
@@ -81,103 +106,139 @@ class TaskFrame extends React.Component {
       }
     `;
 
-    const handleClick = (task) => {
-      this.props.history.push(`/panel/tasks/manage-task/${task? task.surveyID: ""}` );
-
-    }
-    
     const columns = [
-      { 
-        name: "Task Title",
+      {
+        name: 'Task Title',
         options: {
-          filter: false
-        }
+          filter: false,
+        },
       },
       {
-        name: "Last Updated",
+        label: 'Featured',
+        name: 'Featured',
         options: {
-          filter: false
-        }
+          filter: true,
+          sort: false,
+          customBodyRender: featured => {
+            return (
+              featured && <Chip icon={<StarRoundedIcon />} label="Featured" variant="outlined" />
+            );
+          },
+          filterOptions: {
+            names: ['Featured', 'Not Featured'],
+            logic(featured, filterVal) {
+              const show =
+                (filterVal.indexOf('Featured') >= 0 && featured === true) ||
+                (filterVal.indexOf('Not Featured') >= 0 && featured !== true);
+              return !show;
+            },
+          },
+        },
       },
       {
-        name: "Created By",
+        name: 'Last Updated',
         options: {
-          filter: false
-        }
+          filter: false,
+        },
       },
       {
-        name: "Stage",
+        name: 'Created By',
+        options: {
+          filter: false,
+        },
+      },
+      {
+        name: 'Stage',
         options: {
           filter: true,
           filterOptions: {
-            names: ['Active', "InActive"],
+            names: ['Active', 'InActive'],
             logic(stage, filterVal) {
-              const show = (filterVal.indexOf("Active") >= 0 && stage == "Active") || (filterVal.indexOf("InActive") >= 0 && stage != "Active");
-              return !show
-            }
+              const show =
+                (filterVal.indexOf('Active') >= 0 && stage === 'Active') ||
+                (filterVal.indexOf('InActive') >= 0 && stage !== 'Active');
+              return !show;
+            },
           },
-        }
-      }
+        },
+      },
     ];
 
-    const data = TaskStore.allTasks.map(task => [task.label, UTCtoFriendly(task.updated), AccountStore._getDisplayName(task.userID), task.active? "Active":"Inactive"]);
+    const data = TaskStore.allTasks.map(task => [
+      task.label,
+      task.featured,
+      UTCtoFriendly(task.updated),
+      AccountStore._getDisplayName(task.userID),
+      task.active ? 'Active' : 'Inactive',
+    ]);
 
-    const mobileColumns = ["Task Title", "Last Updated"];
+    const mobileColumns = ['Task Title', 'Last Updated'];
 
     const mobileData = TaskStore.allTasks.map(task => [task.label, UTCtoFriendly(task.updated)]);
 
     const options = {
       elevation: 1,
-      selectableRows: "none",
-      filter:true,
+      selectableRows: 'multiple',
+      customToolbarSelect: selectedRows => (
+        <CustomToolbarSelect
+          data={TaskStore.allTasks}
+          selectedRows={selectedRows}
+          handleClick={(e, v) => {
+            this.handleFeatured(e, v);
+          }}
+        />
+      ),
+      filter: true,
       filterType: 'dropdown',
-      // filterList: [["active"]],
       print: false,
-      responsive: "scrollMaxHeight",
+      responsive: 'scrollMaxHeight',
       viewColumns: false,
       download: false,
-
-      onRowClick: (i, data) => handleClick(TaskStore.allTasks[data.dataIndex])
+      onRowClick: (i, rowData) => {
+        this.handleClick(TaskStore.allTasks[rowData.dataIndex]);
+      },
     };
 
     const { width } = this.state;
 
     return (
-      <React.Fragment>
-        <Header as="h2" style={{ padding: 0, margin: "10px 0 10px" }}>
+      <>
+        <Header
+          as="h2"
+          style={{
+            padding: 0,
+            margin: '10px 0 10px',
+          }}
+        >
           Tasks
-          <Header.Subheader>
-            Assign tasks to better track progress
-          </Header.Subheader>
+          <Header.Subheader>Assign tasks to better track progress</Header.Subheader>
         </Header>
         <MenuContainer>
-          <div style={{ textAlign: "center" }}>
-            <Button color="blue" onClick={()=>handleClick()}>
-              {" "}
-              <Icon name="plus" /> Create New...{" "}
+          <div style={{ textAlign: 'center' }}>
+            <Button
+              color="blue"
+              onClick={() => {
+                this.handleClick();
+              }}
+            >
+              {' '}
+              <Icon name="plus" />
+              Create New..
+              {' '}
             </Button>
           </div>
         </MenuContainer>
-        <span>
-  </span>      
+        <span />
         <div style={{ marginTop: 15 }}>
-        <MuiThemeProvider theme={this.getMuiTheme()}>
-          { 
-            width > 380?  <MUIDataTable
-                            data={data}
-                            columns={columns}
-                            options={options}
-                          />
-                        :
-                          <MUIDataTable
-                            data={mobileData}
-                            columns={mobileColumns}
-                            options={options}
-                          />
-          }     
+          <MuiThemeProvider theme={this.getMuiTheme()}>
+            {width > 380 ? (
+              <MUIDataTable data={data} columns={columns} options={options} />
+            ) : (
+              <MUIDataTable data={mobileData} columns={mobileColumns} options={options} />
+            )}
           </MuiThemeProvider>
         </div>
-      </React.Fragment>
+      </>
     );
   }
 }
