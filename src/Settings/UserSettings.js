6f357c5ad1424c681f543f53c77f9cc33250d47a
fancykingstar <fancykingstar2019@gmail.com>
2020-01-11 04:02:50 -0500

    add FixSemanticLabel to className

--- src/Settings/UserSettings.js
+++ src/Settings/UserSettings.js
@@ -95,24 +95,27 @@ export class UserSettings extends React.Component {
         />
 
         <Segment>
-          <div style={{ maxWidth: 400 }}>
+          <div>
             <Form>
-            <Form.Input
-                label="Full Name"
-                value={DataEntryStore.userSettings.displayName_full}
-                onChange={(e, val) => handleFullName(val.value)}
-              >
-                {" "}
-                <input maxLength="32" />{" "}
-              </Form.Input>
-              <Form.Input
-                label={<span>Display Name<InfoPopUp content="Short name or nickname"/></span>}
-                value={DataEntryStore.userSettings.displayName}
-                onChange={(e, val) => handleName(val.value)}
-              >
-                {" "}
-                <input maxLength="32" />{" "} </Form.Input>
-             
+              <div style={{ maxWidth: 400, float: "left"}}>
+                <Form.Input className="FixSemanticLabel"
+                  label="Full Name"
+                  value={DataEntryStore.userSettings.displayName_full}
+                  onChange={(e, val) => handleFullName(val.value)}
+                >
+                  {" "}
+                  <input maxLength="32" />{" "}
+                </Form.Input>
+
+                <Form.Input className="FixSemanticLabel"
+                  label={<span>Display Name<InfoPopUp content="Short name or nickname"/></span>}
+                  value={DataEntryStore.userSettings.displayName}
+                  onChange={(e, val) => handleName(val.value)}
+                >
+                  {" "}
+                  <input maxLength="32" />{" "} 
+                </Form.Input>
+                 
                 {/* <Form.Select
                   label="Default Timezone"
                   options={timezones}
@@ -120,94 +123,111 @@ export class UserSettings extends React.Component {
                   onChange={(e, { value }) => handleTimezone(value)}
                   search
                 /> */}
-              <Form.Input
-                label={"Title"}
-                value={DataEntryStore.userSettings.Title}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "Title", val.value)}    
-               >     <input maxLength="32" />{" "} </Form.Input>
-              <Form.Input
-                icon="building"
-                label={"Department"}
-                value={DataEntryStore.userSettings.Department}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "Department", val.value)}
-                maxLength="32"
-               />
-             <Form.Input
-                icon="map marker alternate"
-                label={"Location"}
-                value={DataEntryStore.userSettings.Location}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "Location", val.value)}
-                maxLength="32"
-               />
-              <Form.Input icon="phone"
-                type="number"
-                maxLength="13"
-                label={"Phone or Extension"}
-                value={DataEntryStore.userSettings["Phone or Extension"]}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "Phone or Extension", val.value)}    
-               />
-               <Form.Input icon="mobile alternate"
-                type="number"
-                maxLength="13"
-                label={"Mobile"}
-                value={DataEntryStore.userSettings["Mobile"]}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "Mobile", val.value)}    
-               />
-            <Form.Input icon="birthday cake"
-              type="date"
-              label="Birthday"
-              value={DataEntryStore.userSettings.dob}
-              onChange={(e) => DataEntryStore.set("userSettings", "dob", e.target.value)}    />
-              <Form.TextArea
-                label={"About Me"}
-                value={DataEntryStore.userSettings["About Me"]}
-                onChange={(e, {value}) => DataEntryStore.set("userSettings", "About Me", value.split("\n").join("")) }
-                > <input maxLength="256" />{" "} </Form.TextArea>     
-            <Form.Input label={ <span> <Icon name={"twitter"} /> {"Twitter"}{" "} </span> } > {" "}
-            <Input
-                label={"@"}
-                maxLength="32"
-                value={DataEntryStore.userSettings["Twitter"]}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "Twitter", val.value)}    
-               /> 
-            </Form.Input>   
 
-            <Form.Input label={ <span> <Icon name={"medium"} /> {"Medium"}{" "} </span> } > {" "}
-            <Input
-                fluid
-                maxLength="62"
-                label={"https://medium.com/@"}
-                value={DataEntryStore.userSettings["Medium"]}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "Medium", val.value)}    
-               /> 
-            </Form.Input>    
+                <Form.Input className="FixSemanticLabel"
+                  label={"Title"}
+                  value={DataEntryStore.userSettings.Title}
+                  onChange={(e, val) => DataEntryStore.set("userSettings", "Title", val.value)}    
+                 >     
+                  <input maxLength="32" />
+                  {" "}
+                </Form.Input>
 
-            <Form.Input label={ <span> <Icon name={"github"} /> {"Github"}{" "} </span> } > {" "}
-            <Input
-                fluid
-                maxLength="62"
-                label={"https://github.com/"}
-                value={DataEntryStore.userSettings["Github"]}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "Github", val.value)}    
-               /> 
-            </Form.Input>    
+                <Form.Input className="FixSemanticLabel"
+                  icon="building"
+                  label={"Department"}
+                  value={DataEntryStore.userSettings.Department}
+                  onChange={(e, val) => DataEntryStore.set("userSettings", "Department", val.value)}
+                  maxLength="32"
+                 />
 
-            <Form.Input label={ <span> <Icon name={"linkedin"} /> {"LinkedIn"}{" "} </span> } > {" "}
-            <Input
-                fluid
-                maxLength="62"
-                label={"https://linkedin.com/"}
-                value={DataEntryStore.userSettings["LinkedIn"]}
-                onChange={(e, val) => DataEntryStore.set("userSettings", "LinkedIn", val.value)}    
-               /> 
-            </Form.Input>    
-<br/>
-              <Button primary type="submit"
-              disabled={DataEntryStore.userSettings.displayName === "" || DataEntryStore.userSettings.displayName_full === ""}
-              onClick={e => modifyUser(userSettingsEdit())}
-              >
-                Update
-              </Button>
+                <Form.Input className="FixSemanticLabel"
+                  icon="map marker alternate"
+                  label={"Location"}
+                  value={DataEntryStore.userSettings.Location}
+                  onChange={(e, val) => DataEntryStore.set("userSettings", "Location", val.value)}
+                  maxLength="32"
+                />
+
+                <Form.Input className="FixSemanticLabel" icon="phone"
+                  type="number"
+                  maxLength="13"
+                  label={"Phone or Extension"}
+                  value={DataEntryStore.userSettings["Phone or Extension"]}
+                  onChange={(e, val) => DataEntryStore.set("userSettings", "Phone or Extension", val.value)}    
+                />
+
+                <Form.Input className="FixSemanticLabel" icon="mobile alternate"
+                  type="number"
+                  maxLength="13"
+                  label={"Mobile"}
+                  value={DataEntryStore.userSettings["Mobile"]}
+                  onChange={(e, val) => DataEntryStore.set("userSettings", "Mobile", val.value)}    
+                />
+
+                <Form.Input className="FixSemanticLabel" icon="birthday cake"
+                  type="date"
+                  label="Birthday"
+                  value={DataEntryStore.userSettings.dob}
+                  onChange={(e) => DataEntryStore.set("userSettings", "dob", e.target.value)}
+                />
+
+                <Form.Input className="FixSemanticLabel" label={ <span> <Icon name={"twitter"} /> {"Twitter"}{" "} </span> } >
+                  {" "}
+                  <Input
+                    label={"@"}
+                    maxLength="32"
+                    value={DataEntryStore.userSettings["Twitter"]}
+                    onChange={(e, val) => DataEntryStore.set("userSettings", "Twitter", val.value)}    
+                   /> 
+                </Form.Input>   
+
+                <Form.Input className="FixSemanticLabel" label={ <span> <Icon name={"medium"} /> {"Medium"}{" "} </span> } > {" "}
+                  <Input
+                    fluid
+                    maxLength="62"
+                    label={"https://medium.com/@"}
+                    value={DataEntryStore.userSettings["Medium"]}
+                    onChange={(e, val) => DataEntryStore.set("userSettings", "Medium", val.value)}    
+                   /> 
+                </Form.Input>    
+
+                <Form.Input className="FixSemanticLabel" label={ <span> <Icon name={"github"} /> {"Github"}{" "} </span> } > {" "}
+                  <Input
+                    fluid
+                    maxLength="62"
+                    label={"https://github.com/"}
+                    value={DataEntryStore.userSettings["Github"]}
+                    onChange={(e, val) => DataEntryStore.set("userSettings", "Github", val.value)}    
+                   /> 
+                </Form.Input>    
+
+                <Form.Input className="FixSemanticLabel" label={ <span> <Icon name={"linkedin"} /> {"LinkedIn"}{" "} </span> } > {" "}
+                  <Input
+                    fluid
+                    maxLength="62"
+                    label={"https://linkedin.com/"}
+                    value={DataEntryStore.userSettings["LinkedIn"]}
+                    onChange={(e, val) => DataEntryStore.set("userSettings", "LinkedIn", val.value)}    
+                   /> 
+                </Form.Input>    
+                <br/>
+
+                <Button primary type="submit"
+                  disabled={DataEntryStore.userSettings.displayName === "" || DataEntryStore.userSettings.displayName_full === ""}
+                  onClick={e => modifyUser(userSettingsEdit())}
+                >
+                  Update
+                </Button>
+              </div>
+              <div style={{ maxWidth: 400, float: "left"}}>
+                <Form.TextArea
+                  label={"About Me"}
+                  value={DataEntryStore.userSettings["About Me"]}
+                  onChange={(e, {value}) => DataEntryStore.set("userSettings", "About Me", value.split("\n").join("")) }> 
+                  <input maxLength="256" />{" "}
+                </Form.TextArea>
+              </div>
             </Form>
             <Message error attached hidden={newLabelStatus_full.messageHide}>
               {newLabelStatus_full.message}
@@ -223,7 +243,7 @@ export class UserSettings extends React.Component {
           {!this.state.changePassword?
           <Form>
           <Form.Group>
-              <Form.Input
+              <Form.Input className="FixSemanticLabel"
               disabled={true}
               label="Email"
               value={UserStore.user.email}
@@ -237,8 +257,8 @@ export class UserSettings extends React.Component {
           <>
             <Form>
               <Form.Group>
-              <Form.Input icon="key" type="password"  label="New password" onChange={(e) => this.setState({pwd1: e.target.value})}/>
-              <Form.Input icon="key" type="password"  label="Confirm new password" onChange={(e) => this.setState({pwd2: e.target.value})}/>
+              <Form.Input className="FixSemanticLabel" icon="key" type="password"  label="New password" onChange={(e) => this.setState({pwd1: e.target.value})}/>
+              <Form.Input className="FixSemanticLabel" icon="key" type="password"  label="Confirm new password" onChange={(e) => this.setState({pwd2: e.target.value})}/>
           
               </Form.Group>
               <Button primary size="small"  onClick={e =>  this.validatePwd() }>Submit</Button>
